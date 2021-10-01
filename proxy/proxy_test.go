b527b1273a5d6606fc0bf02450c0704204c5261f
Daniel Thomas <daniel.thomas@dantho.de>
2021-10-01 10:20:04 +0200

    add test for execution of callbacks after flush

--- proxy/proxy_test.go
+++ proxy/proxy_test.go
@@ -2,23 +2,27 @@ package proxy
 
 import (
 	"context"
+	"go.uber.org/zap"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 const (
 	responseFoo = `i am a foo response`
-	responseBar = `i am bar`
+	responseBar = `i am bar response`
+	responseUpdate = `update`
+	responseFlush = `flush`
 )
 
 type getStats func(path string) int
 
-func GetBackend() (getStats, http.HandlerFunc) {
+func GetBackend(t *testing.T) (getStats, http.HandlerFunc) {
 	stats := map[string]int{}
 	statLock := sync.RWMutex{}
 	return func(path string) int {
@@ -34,6 +38,8 @@ func GetBackend() (getStats, http.HandlerFunc) {
 			stats[r.URL.Path]++
 			statLock.Unlock()
 
+			t.Log("backend: url called", r.URL.Path)
+
 			switch r.URL.Path {
 			case "/foo":
 				w.Write([]byte(responseFoo))
@@ -46,21 +52,65 @@ func GetBackend() (getStats, http.HandlerFunc) {
 		}
 }
 
-func getTestServer(t *testing.T) (gs func(path string) int, s *httptest.Server) {
-	gs, backendHandler := GetBackend()
+func GetWebHook(t *testing.T) (getStats, http.HandlerFunc) {
+	stats := map[string]int{}
+	statLock := sync.RWMutex{}
+	return func(path string) int {
+			statLock.RLock()
+			defer statLock.RUnlock()
+			count, ok := stats[path]
+			if !ok {
+				return -1
+			}
+			return count
+		}, func(w http.ResponseWriter, r *http.Request) {
+			statLock.Lock()
+			stats[r.URL.Path]++
+			statLock.Unlock()
 
-	p := NewProxy(context.Background(), httptest.NewServer(backendHandler).URL)
+			t.Log("webhook: url called", r.URL.Path)
+
+			switch r.URL.Path {
+			case "/update":
+				w.Write([]byte(responseUpdate))
+				return
+			case "/flush":
+				w.Write([]byte(responseFlush))
+				return
+			}
+			http.Error(w, "not found", http.StatusNotFound)
+		}
+}
+
+func getTestServer(t *testing.T) (gs func(path string) int, ws func(path string) int, s *httptest.Server) {
+
+	l, _ := zap.NewProduction()
+
+	gs, backendHandler := GetBackend(t)
+	ws, webHookHandler := GetWebHook(t)
+	backendServer := httptest.NewServer(backendHandler)
+	webHookServer := httptest.NewServer(webHookHandler)
+
+	p := NewProxy(
+		context.Background(),
+		l,
+		backendServer.URL,
+		[]WebHookURL{
+			WebHookURL(webHookServer.URL + "/update"),
+			WebHookURL(webHookServer.URL + "/flush"),
+		},
+	)
 	s = httptest.NewServer(p)
 	t.Log("we have a proxy in front of it running on", s.URL)
-	return gs, s
+	return gs, ws, s
 
 }
 
 func TestProxy(t *testing.T) {
-	gs, server := getTestServer(t)
+	gs, ws, server := getTestServer(t)
 
 	get := func(path string) string {
-		resp, err := http.Get(server.URL + "/foo")
+		resp, err := http.Get(server.URL + path)
 		assert.NoError(t, err)
 		defer resp.Body.Close()
 		responseBytes, err := ioutil.ReadAll(resp.Body)
@@ -69,7 +119,7 @@ func TestProxy(t *testing.T) {
 	}
 	for j := 0; j < 10; j++ {
 		wg := sync.WaitGroup{}
-		for i := 0; i < 128; i++ {
+		for i := 0; i < 1; i++ {
 			wg.Add(1)
 			go func() {
 				get("/foo")
@@ -79,5 +129,13 @@ func TestProxy(t *testing.T) {
 		wg.Wait()
 	}
 	assert.Equal(t, 1, gs("/foo"))
+
+	//
+	http.Get(server.URL + "/api/flush")
+	time.Sleep(time.Second * 1)
+	////t.Log(server.URL)
+	//
+	assert.Equal(t, 1, ws("/update"))
+	assert.Equal(t, 1, ws("/flush"))
 
 }
